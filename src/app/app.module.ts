6233e3899f0a8b8e1b7db55fb12cd1fb1027ff4c
Wandu <d_wandz@yahoo.com>
2019-03-21 11:18:15 +0200

    navbar add, communicate between angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event.thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventListComponent
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'events-list',
-    templateUrl: './events-list.component.html'
+    template: `
+    <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr>
+    <event-thumbnail [event]="event1"></event-thumbnail>
+</div>`
 })
 export class EventListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '26/9/2020',
